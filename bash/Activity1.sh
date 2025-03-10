7a6baa022eae68df5a10af15e3f9e8bb5cb0c25b
bgduque <boris.dduque@gmail.com>
2025-03-10 23:56:27 +0800

    🚀 Initial Commit

---
+++ bash/Activity1.sh
@@ -0,0 +1,10 @@
+echo "Enter your name: "
+read NAME
+
+echo "Enter your age: "
+read AGE
+
+echo "Enter your program: "
+read PROGRAM
+
+echo "Hello $NAME!  your age is $AGE, your program is $PROGRAM."
